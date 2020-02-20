dd860d285dd56464e502fc78f426d4ea4f7add97
kern.yoo <trustyoo86@gmail.com>
2020-02-21 00:06:24 +0900

    Add monorepo next.js package

--- packages/main/next.config.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  distDir: path.resolve(__dirname, '..', '..', 'dist'),
-};
--- packages/main/server.js
+++ packages/main/server.js
@@ -1,13 +1,10 @@
 'use strict';
 
 const next = require('next');
-const app = next({ dev: true });
 
-function route(server, root) {
-  app.prepare().then(() => {
-    server.get('/main', (req, res) => {
-      app.render(req, res, '/main', req.query);
-    });
+function route(server, app) {
+  server.get('/main', (req, res) => {
+    app.render(req, res, '/main', req.query);
   });
 }
 
